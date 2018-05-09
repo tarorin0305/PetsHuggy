e72595f4d79d2a034e7bbee539832fdf5f5523d0
vagrant <vagrant@localhost.localdomain>
2018-05-09 22:31:39 +0000

    delete AWS_HOST key from production.rb and development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   s3_protocol: :https,
   bucket: ENV['S3_BUCKET_NAME'],
   s3_region: ENV['AWS_REGION'],
-  s3_host_name: ENV['AWS_HOST'],
+  # s3_host_name: ENV['AWS_HOST'],
   s3_credentials: {
      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
     s3_protocol: :https,
     bucket: ENV.fetch('S3_BUCKET_NAME'),
     s3_region: ENV.fetch('AWS_REGION'),
-    s3_host_name: ENV.fetch('AWS_HOST'),
+    # s3_host_name: ENV.fetch('AWS_HOST'),
     s3_credentials: {
     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
