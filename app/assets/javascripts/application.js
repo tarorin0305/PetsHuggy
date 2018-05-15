f17e48de53f8e727f2fc1b7f9e35ea23bb4d245b
vagrant <vagrant@localhost.localdomain>
2018-05-15 16:27:30 +0000

    modify jquery-ui/i18n/datepicker-ja

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require dropzone
 //= require bootstrap-sprockets
 //= require jquery-ui/widgets/datepicker
-//= require jquery-ui/widgets/datepicker-ja
+//= require jquery-ui/i18n/datepicker-ja
 //= require toastr
 //= require jquery_ujs
 //= require_tree .
